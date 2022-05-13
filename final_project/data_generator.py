47df2de8e4426a8e71a94bd4ced4ac8523f96fab
JuLieAlgebra <julieannabacon2014@gmail.com>
2022-05-12 21:37:08 -0400

    feat(training, data_generator): worked on features in both of these files. Re-wrote some model handling code in training.py and added model architecture. Modified some data generation in data_generator.

--- final_project/data_generator.py
+++ final_project/data_generator.py
@@ -10,17 +10,23 @@ abs_path = os.path.dirname(__file__)
 
 
 @dataclass
-class DataGenerator(tf.keras.utils.Sequence):  # utils.Sequence):
-    """Generator for Keras training with model.fit_generator"""
+class DataGenerator(tf.keras.utils.Sequence):
+    """
+    Generator for Keras training to allow multiprocessing and training on batches with only the
+    batch itself being loaded into memory.
+
+    This implementation was heavily inspired by various examples for creating data generators
+    for keras, namely https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly.
+    """
 
     data_filenames: list
     labels: list
     n_classes: int
     batch_size: int = 32
-    dim: tuple = (5, 32, 32)
+    dim: tuple = (32, 32, 5)
 
     def __len__(self):
-        """Denotes the number of batches per epoch"""
+        """Number of batches per epoch"""
         return len(self.data_filenames) // self.batch_size
 
     def __getitem__(self, index):
@@ -41,116 +47,26 @@ class DataGenerator(tf.keras.utils.Sequence):  # utils.Sequence):
         # check accuracy, if better, save?
 
     def _data_generation(self, batch):
-        """Generates data containing batch_size samples"""
-        # Initialization
-        X = np.empty((self.batch_size, *self.dim))
-        y = np.empty((self.batch_size), dtype=int)
+        """From file names for a batch, generate the data"""
+        X = np.zeros((self.batch_size, *self.dim))
+        y = np.zeros((self.batch_size), dtype=int)
 
         # Generate data
         for i, ID in enumerate(batch):
-            try:  # TODO remove, artifact of not every obj in csv being downloaded & processed
-                # Store sample
-                x = glob.glob("data/processed/" + str(ID) + "-*.npy")[0]
-                X[
-                    i,
-                ] = np.load(x)
+            x = glob.glob("data/processed/" + str(ID) + "-*.npy")[0]
+            X[
+                i,
+            ] = np.load(x).T
 
-                # Store class
+            # small bug here from how I've integrated the model architecture from Pasquet.
+            if type(self.labels[ID]) != float and type(self.labels[ID]) != np.float64:
+                print(type(self.labels[ID]))
+            else:
                 y[i] = self.labels[ID]
-            except KeyError:
-                pass
-        print("KERA S UTTILS CATEGORIACAL : ", tf.keras.utils.to_categorical(y, num_classes=self.n_classes))
-        return X, tf.keras.utils.to_categorical(y, num_classes=self.n_classes)
 
-
-# import numpy as np
-
-# from tf.keras.models import Sequential
-# from my_classes import DataGenerator
-
-# # Parameters
-# params = {'dim': (32,32,32),
-#           'batch_size': 64,
-#           'n_classes': 6,
-#           'n_channels': 1,
-#           'shuffle': True}
-
-# # Datasets
-# partition = # IDs
-# labels = # Labels
-
-# # Generators
-# training_generator = DataGenerator(partition['train'], labels, **params)
-# validation_generator = DataGenerator(partition['validation'], labels, **params)
-
-# # Design model
-# model = Sequential()
-# [...] # Architecture
-# model.compile()
-
-# # Train model on dataset
-# model.fit_generator(generator=training_generator,
-#                     validation_data=validation_generator,
-#                     use_multiprocessing=True,
-#                     workers=6)
+        return X, tf.keras.utils.to_categorical(y * self.n_classes, num_classes=self.n_classes)
+        # np.array(list(map(np.argwhere, tf.keras.utils.to_categorical(y*self.n_classes, num_classes=self.n_classes))))
 
 
 # From
 # https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
-
-"""
-class DataGenerator(tf.keras.utils.Sequence):
-    "Generates data for Keras"
-
-    def __init__(self, list_IDs, labels, batch_size=32, dim=(5, 32, 32), n_channels=1, n_classes=100, shuffle=True):
-        "Initialization"
-        self.dim = dim
-        self.batch_size = batch_size
-        self.labels = labels
-        self.list_IDs = list_IDs
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.shuffle = shuffle
-        self.on_epoch_end()
-
-    def __len__(self):
-        "Denotes the number of batches per epoch"
-        return int(np.floor(len(self.list_IDs) / self.batch_size))
-
-    def __getitem__(self, index):
-        "Generate one batch of data"
-        # Generate indexes of the batch
-        indexes = self.indexes[index * self.batch_size : (index + 1) * self.batch_size]
-
-        # Find list of IDs
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
-
-        # Generate data
-        X, y = self._data_generation(list_IDs_temp)
-
-        return X, y
-
-    def on_epoch_end(self):
-        "Updates indexes after each epoch"
-        self.indexes = np.arange(len(self.list_IDs))
-        if self.shuffle == True:
-            np.random.shuffle(self.indexes)
-
-    def _data_generation(self, list_IDs_temp):
-        "Generates data containing batch_size samples"  # X : (n_samples, *dim, n_channels)
-        # Initialization
-        X = np.empty((self.batch_size, *self.dim, self.n_channels))
-        y = np.empty((self.batch_size), dtype=int)
-
-        # Generate data
-        for i, ID in enumerate(list_IDs_temp):
-            # Store sample
-            X[
-                i,
-            ] = np.load("data/" + ID + ".npy")
-
-            # Store class
-            y[i] = self.labels[ID]
-
-        return X, tf.keras.utils.to_categorical(y, num_classes=self.n_classes)
-"""
